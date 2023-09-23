2c234332e1ebe8975de0135962b25dd65ff392e3
Kriti <65589492+kriti-karna@users.noreply.github.com>
2023-09-23 14:44:37 +0545

    some changes

--- TodoFinal/ReactApi/react_api/src/http-common.ts
+++ TodoFinal/ReactApi/react_api/src/http-common.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5013/api/Todo/",
+  //baseURL: "http://localhost:5013/api/Todo/",
+  baseURL: "https://krititodo.azurewebsites.net/api/Todo/",
   headers: {
     "Content-type": "application/json"
   }
